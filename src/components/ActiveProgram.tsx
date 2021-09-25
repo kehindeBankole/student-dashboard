62c4a257bc4939e39b6847d2ab814264a246c7f0
kehindeBankole <bankolek1@gmail.com>
2021-09-26 00:15:10 +0100

    more changes

---
+++ src/components/ActiveProgram.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ActiveProgram.scss'
+function ActiveProgram() {
+    return (
+        <div className="card-container">
+            
+        </div>
+    )
+}
+
+export default ActiveProgram
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
-
+import React from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import "./Landing.scss";
+import { ReactComponent as CustomizeIcon } from "../assets/customise.svg";
+import ActiveProgram from "../components/ActiveProgram";
 function Landing() {
-    return (
-        <div>
-           <nav>
-               <Sidebar />
-           </nav>
-           <main>
-               <Header />
-           </main>
+  return (
+    <div>
+      <nav>
+        <Sidebar />
+      </nav>
+      <main>
+        <Header />
+        <div className="main-container">
+          <div className="main-container_left">
+            <div className="name-bar">
+              <h3>Hello, Mia!</h3>
+              <p>
+                {" "}
+                <CustomizeIcon className="customize" />
+                Customize
+              </p>
+            </div>
+            <ActiveProgram/>
+          </div>
+          <div className="main-container_right"></div>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
